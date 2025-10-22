583dc026d0a32225237c6144dceb59148e5e11f2
Anthony <theonlyway@users.noreply.github.com>
2025-10-22 08:55:24 +0000

    working on ado logic

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package cmd
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"os"
 
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v7"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/core"
 	"github.com/theonlyway/avm-module-sync/internal/avmmodules"
 	"github.com/theonlyway/avm-module-sync/internal/config"
 	"go.uber.org/zap"
@@ -19,6 +23,7 @@ func Execute() {
 	flag.StringVar(&config.AdoProject, "ado-project", "", "The ADO project")
 	flag.StringVar(&config.AdoRepo, "ado-repo", "", "The ADO repository")
 	flag.StringVar(&config.AdoPat, "ado-pat", "", "The ADO personal access token")
+
 	flag.BoolVar(&config.DebugMode, "debug", false, "Enable debug mode")
 
 	logger, _ := zap.NewProduction()
@@ -28,6 +33,23 @@ func Execute() {
 	parseRequiredFlags()
 
 	processor := avmmodules.ModuleProcessor{Logger: logger, SugaredLogger: sugaredLogger}
+
+	organizationUrl := config.AdoOrganizationUrl + config.AdoOrganization
+
+	// Create a connection to your organization
+	connection := azuredevops.NewPatConnection(organizationUrl, config.AdoPat)
+
+	ctx := context.Background()
+
+	// Create a client to interact with the Core area
+	coreClient, err := core.NewClient(ctx, connection)
+	if err != nil {
+		logger.Error("Failed to create client", zap.Error(err))
+		os.Exit(1)
+	}
+
+	coreClient.GetProjects(ctx, core.GetProjectsArgs{})
+
 
 	if config.ProcessResourceModules {
 		sugaredLogger.Infow("resource modules:")
@@ -74,5 +96,8 @@ func parseRequiredFlags() {
 	}
 	if config.AdoRepo == "" {
 		panic("ADO repository is required. Use -ado-repo to specify it.")
+	}
+	if config.AdoPat == "" {
+		panic("ADO personal access token is required. Use -ado-pat to specify it.")
 	}
 }
